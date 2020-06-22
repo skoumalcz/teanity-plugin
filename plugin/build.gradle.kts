62118584a0c361662b03e882d83b865b8c3b9dff
Viktor De Pasquale <info@depasquale.wiki>
2020-06-22 10:07:39 +0200

    Downgraded the build tools
    The rewrite that happened in 4.1.* crippled the version functionality, so it's discouraged to use the newer version until it's resolved.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     compileOnly(gradleApi())
 
-    implementation("com.android.tools.build:gradle:4.1.0-beta01")
+    implementation("com.android.tools.build:gradle:4.0.0")
     implementation(kotlin("gradle-plugin", "1.3.72"))
     implementation(kotlin("android-extensions"))
 }
